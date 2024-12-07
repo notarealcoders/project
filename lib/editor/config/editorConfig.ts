c7d02c8e0896801a4c651d2a907498604655bcd6
notarealcoders <rohitsingh.code@gmail.com>
2024-12-07 11:32:38 +0530

    cursorSmoothCaretAnimation: 'on'

--- lib/editor/config/editorConfig.ts
+++ lib/editor/config/editorConfig.ts
@@ -1,25 +1,26 @@
-import { editor } from 'monaco-editor';
+import { editor } from "monaco-editor";
 
-export const defaultEditorOptions: editor.IStandaloneEditorConstructionOptions = {
-  minimap: { enabled: false },
-  fontSize: 14,
-  wordWrap: 'on',
-  automaticLayout: true,
-  lineNumbers: 'on',
-  roundedSelection: true,
-  scrollBeyondLastLine: false,
-  cursorBlinking: 'smooth',
-  cursorSmoothCaretAnimation: true,
-  smoothScrolling: true,
-  multiCursorModifier: 'ctrlCmd',
-  autoClosingBrackets: 'always',
-  autoClosingQuotes: 'always',
-  formatOnPaste: true,
-  formatOnType: true,
-};
+export const defaultEditorOptions: editor.IStandaloneEditorConstructionOptions =
+  {
+    minimap: { enabled: false },
+    fontSize: 14,
+    wordWrap: "on",
+    automaticLayout: true,
+    lineNumbers: "on",
+    roundedSelection: true,
+    scrollBeyondLastLine: false,
+    cursorBlinking: "smooth",
+    cursorSmoothCaretAnimation: "on",
+    smoothScrolling: true,
+    multiCursorModifier: "ctrlCmd",
+    autoClosingBrackets: "always",
+    autoClosingQuotes: "always",
+    formatOnPaste: true,
+    formatOnType: true,
+  };
 
 export const monacoLoaderConfig = {
   paths: {
-    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs',
+    vs: "https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs",
   },
 };
